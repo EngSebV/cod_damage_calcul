2850db1f3e50acc3030aaeed8f87b3b8c720cd44
SevC <progsevcode@gmail.com>
2025-03-09 17:23:30 -0500

    2 decimal cod_dmg_calculator.cpp

--- cpp_version/cod_dmg_calculator.cpp
+++ cpp_version/cod_dmg_calculator.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>  
 #include <string>
 
 using namespace std;
@@ -10,19 +11,18 @@ int main() {
     cout << "Welcome to COD damage calculator. The materials are as follows: "
          << "Wood = 1, Metal = 2, Concrete = 3. If you don't know the material, type 0." << endl;
 
-   
     cout << "Enter base damage: ";
     cin >> base_damage;
 
-    
     cout << "Enter material type (1, 2, 3, or 0): ";
     cin >> material_type;
 
-    
     if (material_type != 1 && material_type != 2 && material_type != 3 && material_type != 0) {
         cout << "Error: Invalid material type. If you don't know the material, type 0." << endl;
     } else {
-        
+       
+        cout << fixed << setprecision(2);
+
         if (material_type == 1) {  // Wood
             resulting_damage = base_damage - (0.3 * base_damage);
             cout << "Resulting damage: " << resulting_damage << endl;
@@ -36,11 +36,9 @@ int main() {
             cout << "Enter the damage dealt after reduction: ";
             cin >> dealt_damage;
 
-            
             float reduction_percent = (1 - (dealt_damage / base_damage)) * 100;
             string material;
 
-            
             if (reduction_percent == 30) {
                 material = "Wood";
             } else if (reduction_percent == 50) {
